d2d0dfe6955597a9d86476c64049df8aaf4bd4d5
Octaviojv4 <octaviojaviervallejos@gmail.com>
2025-05-17 01:36:41 -0300

    creacion de procedimientos ahorro de energia y reglas de automatizacion, se agrego en main y menu, se creo la funcion auxiliar filtrar_listas en utils

--- utils.py
+++ utils.py
@@ -10,6 +10,16 @@ def mostrar_mensaje_error():
     pass
     # para centralizar los errores.
 
+def filtrar_lista (lista, clave, valor):
+ # toma una lista de directorios y la filtra segun una su clave y valor por el que lo hacemos
+    contador = 0
+    lista_filtrada = []
+    while contador in  range (len(lista)):
+        dispositivo = lista[contador]
+        if dispositivo[clave] == valor:
+             lista_filtrada.append(dispositivo["nombre"])
+        contador += 1
+    return lista_filtrada
 
 if __name__ == "__main__":
     print(generar_id_dispositivo())
