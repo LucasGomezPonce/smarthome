00edbe695faa84612a2241f9150c3457643d31d3
Octaviojv4 <octaviojaviervallejos@gmail.com>
2025-05-17 13:16:06 -0300

    correccion de rango de contador

--- automatizacion.py
+++ automatizacion.py
@@ -3,20 +3,12 @@ from dispositivos import listar_dispositivos
 
 def activar_modo_ahorro(lista_dispositivos):
     contador = 0
-<<<<<<< HEAD
-    while contador in range (len(lista_dispositivos)):
-=======
-    while contador in  range (len(lista_dispositivos)):
->>>>>>> 2d5c204ff9f36512f60b9a515fa9d59c281ad648
+    while contador in range (0,len(lista_dispositivos)):
         dispositivo = lista_dispositivos[contador]
         if dispositivo["tipo"] == "no_esencial":
-            dispositivo["estado"] = "apagado"
+            dispositivo["estado"] ="apagado"
         contador += 1
-<<<<<<< HEAD
-    print("\n Modo 'Ahorro de Energia' Activado \n")
-=======
-    print("\n Modo 'Ahorro de energia' Activado \n")
->>>>>>> 2d5c204ff9f36512f60b9a515fa9d59c281ad648
+        print("\n Modo 'Ahorro de Energia' activado \n")
     #Octavio
     # apaga todos los dispositivos no esenciales.
     # O, si elegís otra automatización(por ejemplo, “modo noche”), una función que aplique esa lógica.
