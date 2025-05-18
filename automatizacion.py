c7b3875c63f22fc2e72bf8084bdad8705236225b
Lucas <lggp_1312@hotmail.com>
2025-05-18 18:02:52 -0300

    solucionando errores de automatizacion

--- automatizacion.py
+++ automatizacion.py
@@ -7,17 +7,19 @@ def activar_modo_ahorro(lista_dispositivos):
     while contador in range(0, len(lista_dispositivos)):
         dispositivo = lista_dispositivos[contador]
         if dispositivo["tipo"] == "no_esencial":
+
             dispositivo["estado"] = "apagado"
         contador += 1
-        print("\n Modo 'Ahorro de Energia' activado \n")
+    print("\n Modo 'Ahorro de Energia' activado \n")
+    print(
+        f"Dispositivos desactivados {filtrar_lista(lista_dispositivos, "tipo", "no_esencial")}")
     # Octavio
     # apaga todos los dispositivos no esenciales.
     # O, si elegís otra automatización(por ejemplo, “modo noche”), una función que aplique esa lógica.
 
 
 def reglas_de_automatizacion(lista_dispositivos):
-    print("\n al activar el modo 'Ahorro de Energia' se desactivaran los dispositivos  de tipo no escenciales:\n")
-    listar_dispositivos(filtrar_lista(
-        lista_dispositivos, "tipo", "no_esencial"))
+    print(
+        f"\n Al activar el modo 'Ahorro de Energia' se desactivaran los dispositivos  de tipo no escenciales: {filtrar_lista(lista_dispositivos, "tipo", "no_esencial")}\n")
     # Octavio
     # indicar los dispositivos a apagarse
